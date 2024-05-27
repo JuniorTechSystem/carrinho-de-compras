91d141ae4d347609a8312b3c47e3dcd3af9c4abd
Junior Bortolanza <gbortolanzajr@gmail.com>
2024-05-27 08:23:57 -0300

    Finalizando carrinho de Compras

--- js/app.js
+++ js/app.js
@@ -1,3 +1,6 @@
+let somaTotalProdutos = 0;
+limpar()
+
 function adicionar(id){
     let adicionarProduto = document.getElementById('produto').value;
     let nomeProduto = produto.split('-')[0];
@@ -9,11 +12,24 @@ function adicionar(id){
     listaDeProdutos.innerHTML = listaDeProdutos.innerHTML + `<section class="carrinho__produtos__produto">
     <span class="texto-azul">${quantidade}x</span> ${nomeProduto} <span class="texto-azul">R$1400</span>
   </section>`;
-        
+
+  somaTotalProdutos = somaTotalProdutos + preço;
+
+   let campoTotal = documet.getElementById('valor-total');
+   campoTotal.textContent = `R$ ${somaTotalProdutos}`;
+   document.getElementById('quantidade').value = 0;
 }
+
+
 
 function limpar(){
+  somaTotalProdutos = 0 ;
+  document.getElementById('listas-produtos').innerHTML = '';
+  documet.getElementById('valor-total').textContent = 'R$ 0';
 
 
 
 }
+
+
+
